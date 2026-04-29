a053777f9831b8dfc8802996d8a4de7e0eda91c4
Charles Esumeh <charles.esumeh@venturegardengroup.com>
2026-04-29 10:19:58 +0100

    Updates to dynamic account payload
    Updated Dynamic account request and response payload

--- AdvanclySDK/VirtualAccount/Dtos/CreateDynamicAccount.cs
+++ AdvanclySDK/VirtualAccount/Dtos/CreateDynamicAccount.cs
@@ -11,17 +11,17 @@ public class CreateDynamicAccountRequest
     /// <summary>
     /// The name to be registered on the dynamic account.
     /// </summary>
-    [JsonPropertyName("account_name")] public string Account_Name { get; set; }
+    [JsonPropertyName("account_name")] public string AccountName { get; set; }
 
     /// <summary>
     /// A unique transaction reference for this dynamic account request.
     /// </summary>
-    [JsonPropertyName("transaction_ref")] public string Transaction_Ref { get; set; }
+    [JsonPropertyName("transaction_ref")] public string TransactionRef { get; set; }
 
     /// <summary>
     /// The transaction amount for which the dynamic account is being created.
     /// </summary>
-    [JsonPropertyName("transaction_amount")] public decimal Transaction_Amount { get; set; }
+    [JsonPropertyName("transaction_amount")] public decimal TransactionAmount { get; set; }
 
     /// <summary>
     /// The duration (in days) for which the dynamic account should remain active.
@@ -47,7 +47,7 @@ public class CreateDynamicAccountResponse
     /// <summary>
     /// The HTTP status code of the response.
     /// </summary>
-    [JsonPropertyName("status_code")] public int Status_Code { get; set; }
+    [JsonPropertyName("status_code")] public int StatusCode { get; set; }
 
     /// <summary>
     /// The dynamic account details data payload.
@@ -63,12 +63,12 @@ public class CreateDynamicAccountData
     /// <summary>
     /// The name registered on the dynamic account.
     /// </summary>
-    [JsonPropertyName("account_name")] public string Account_Name { get; set; }
+    [JsonPropertyName("account_name")] public string AccountName { get; set; }
 
     /// <summary>
     /// The dynamically generated account number.
     /// </summary>
-    [JsonPropertyName("account_number")] public string Account_Number { get; set; }
+    [JsonPropertyName("account_number")] public string AccountNumber { get; set; }
 
     /// <summary>
     /// The duration (in days) for which the dynamic account is active.
@@ -78,5 +78,5 @@ public class CreateDynamicAccountData
     /// <summary>
     /// The expiration date and time of the dynamic account in ISO 8601 format.
     /// </summary>
-    [JsonPropertyName("expiry_datetime")] public string Expiry_DateTime { get; set; }
+    [JsonPropertyName("expiry_datetime")] public string ExpiryDateTime { get; set; }
 }
